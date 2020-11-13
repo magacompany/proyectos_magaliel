8183835516ad9406156362afc41e6a2cb7a1d02f
Magaliel Arredondo <magalielarredondo14@gmail.com>
2020-11-13 12:36:08 -0700

    Iniciamos el proyecto

---
+++ contador.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int i;  //se crea la variable iterador
+	
+printf("contador de 1 al 10");
+
+for(i=0; i<=10; i++) //la variable i comienza en 0  mientras sea menor ò igual a 10 incrementara de 1 en 1
+{
+printf("%i \n",i);
+
+}    //fin del bucle for
+
+
+}
